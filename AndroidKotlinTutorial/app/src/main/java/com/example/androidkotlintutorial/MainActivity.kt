41ee7823dff268cbdbfbb38194fbeecc6d24a28b
svarsstad <sandsvarstad@gmail.com>
2021-10-19 15:52:20 +0200

    added extensions to access view elements directly

--- AndroidKotlinTutorial/app/src/main/java/com/example/androidkotlintutorial/MainActivity.kt
+++ AndroidKotlinTutorial/app/src/main/java/com/example/androidkotlintutorial/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.androidkotlintutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.Scene
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
     }
+
 }
