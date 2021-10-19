fe6231ac395e7ce4a6205812ce4774f6503535fc
svarsstad <sandsvarstad@gmail.com>
2021-10-19 16:19:41 +0200

    added function to the button

--- AndroidKotlinTutorial/app/src/main/java/com/example/androidkotlintutorial/MainActivity.kt
+++ AndroidKotlinTutorial/app/src/main/java/com/example/androidkotlintutorial/MainActivity.kt
@@ -7,10 +7,16 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    val choiceTexts = arrayListOf<String>("Han Solo","Leia","luke","Rey","Chewbacca","Bill Nye")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+        buttonGoFish.setOnClickListener {
+            val rnds:Int = (0..6).random()
+            //R.string.text_view_text //resources.string.*name*       xml: @string/*name*
+            textView.text = choiceTexts[rnds]
+
+        }
     }
 
 }
